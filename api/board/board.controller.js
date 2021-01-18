fad8f63347a2b5fd225c58c3c8c52339d776170a
Adi Magori <adimagori@gmail.com>
2021-01-18 13:14:08 +0200

    first commit

---
+++ api/board/board.controller.js
@@ -0,0 +1,45 @@
+const logger = require('../../services/logger.service')
+const userService = require('../user/user.service')
+const boardService = require('./board.service')
+
+async function getBoards(req, res) {
+    try {
+        const boards = await boardService.query(req.query)
+        res.send(boards)
+    } catch (err) {
+        logger.error('Cannot get boards', err)
+        res.status(500).send({ err: 'Failed to get boards' })
+    }
+}
+
+async function deleteBoard(req, res) {
+    try {
+        await boardService.remove(req.params.id)
+        res.send({ msg: 'Deleted successfully' })
+    } catch (err) {
+        logger.error('Failed to delete board', err)
+        res.status(500).send({ err: 'Failed to delete board' })
+    }
+}
+
+
+async function addBoard(req, res) {
+    try {
+        var board = req.body
+        board.byUserId = req.session.user._id
+        board = await boardService.add(board)
+        board.byUser = req.session.user
+        board.aboutUser = await userService.getById(board.aboutUserId)
+        res.send(board)
+
+    } catch (err) {
+        logger.error('Failed to add board', err)
+        res.status(500).send({ err: 'Failed to add board' })
+    }
+}
+
+module.exports = {
+    getBoards,
+    deleteBoard,
+    addBoard
+}
---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'dbURL': 'mongodb+srv://cardly:admin@cluster0.6klvd.mongodb.net/cardly_db?retryWrites=true&w=majority',
+}
