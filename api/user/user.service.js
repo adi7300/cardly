287b43de718a101afb5819e9ce34d7995afafcdb
mmts12 <56490587+mmts12@users.noreply.github.com>
2021-01-19 12:31:25 +0200

    add login mosh

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -79,7 +79,6 @@ async function update(user) {
             _id: ObjectId(user._id),
             username: user.username,
             fullname: user.fullname,
-            score: user.score
         }
         const collection = await dbService.getCollection('user')
         await collection.updateOne({ '_id': userToSave._id }, { $set: userToSave })
@@ -97,7 +96,6 @@ async function add(user) {
             username: user.username,
             password: user.password,
             fullname: user.fullname,
-            score: user.score || 0
         }
         const collection = await dbService.getCollection('user')
         await collection.insertOne(userToAdd)
