a5d8cf5f0f46c283751246ad564e54cce120c9da
mmts12 <56490587+mmts12@users.noreply.github.com>
2021-01-25 17:16:18 +0200

    fix magori bug mosh

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://cardly:admin@cluster0.6klvd.mongodb.net/cardly_db?retryWrites=true&w=majority',
 }
